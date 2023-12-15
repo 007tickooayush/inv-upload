9d161d24f31f7ee8252cfff340250c5cf5f33fc5
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2023-12-15 12:30:15 +0530

    pyspark setup done

--- backview/app.py
+++ backview/app.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
-import uvicorn
+# import uvicorn
+import findspark
+from pyspark.sql import SparkSession
+
 from dotenv import load_dotenv
 import os
+
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-17-openjdk-amd64"
 
 load_dotenv()  # Load environment variables from .env file
 
 app = FastAPI()
+
+spark = SparkSession.builder.appName("PySpark Mysql Connection").config("spark.jars", "/tmp/mysql-connector-java-8.0.23.jar").getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
+
+url = "jdbc:mysql://mysql:3306/file_handle"
+
+mysql_properties = {
+    "user": "root",
+    "password": "root",
+    "driver": "com.mysql.cj.jdbc.Driver"
+}
+
+table_name = "data"
+
+df = spark.read.jdbc(url,table_name,properties=mysql_properties)
 
 @app.get("/")
 def read_root():
@@ -24,6 +45,6 @@ def get_demo():
     # uvicorn.run(app, host="127.0.0.1", port=int(port))  # Run the server
     # uvicorn.run(app)
 
-print('-------------------------------------- DEMO --------------------------------------')
+print('-------------------------------------- app.py ')
 
-print("============================================ Starting server...")
+print("============================================ Started server ============================================")
